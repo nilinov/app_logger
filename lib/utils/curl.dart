a5b6daa9883ff868996a1da1a2e96ed46b92ccc4
n ilinov <n.ilinov@itmegsatar.com>
2021-07-16 18:47:19 +0700

    add try cath for curl export

--- lib/utils/curl.dart
+++ lib/utils/curl.dart
@@ -1,49 +1,59 @@
 part of app_logger;
 
 String cURLRepresentationDio(RequestOptions options) {
-  List<String> components = ["curl -i"];
-  if (options.method != null && options.method.toUpperCase() == "GET") {
-    components.add("-X ${options.method}");
+  try {
+    List<String> components = ["curl -i"];
+    if (options.method != null && options.method.toUpperCase() == "GET") {
+      components.add("-X ${options.method}");
+    }
+
+    if (options.headers != null) {
+      options.headers.forEach((k, v) {
+        if (k != "Cookie") {
+          components.add("-H \"$k: $v\"");
+        }
+      });
+    }
+
+    var data = json.encode(options.data);
+    if (data != null && data != "null") {
+      components.add("-d $data");
+    }
+
+    components.add("\"${options.uri.toString()}\"");
+
+    return components.join('\\\n\t');
+  } catch (err) {
+    debugPrint(err.toString());
+    return "error export";
   }
-
-  if (options.headers != null) {
-    options.headers.forEach((k, v) {
-      if (k != "Cookie") {
-        components.add("-H \"$k: $v\"");
-      }
-    });
-  }
-
-  var data = json.encode(options.data);
-  if (data != null && data != "null") {
-    components.add("-d $data");
-  }
-
-  components.add("\"${options.uri.toString()}\"");
-
-  return components.join('\\\n\t');
 }
 
 String cURLRepresentationHttp(Http.Request options) {
-  List<String> components = ["curl -i"];
-  if (options.method != null && options.method.toUpperCase() == "GET") {
-    components.add("-X ${options.method}");
+  try {
+    List<String> components = ["curl -i"];
+    if (options.method != null && options.method.toUpperCase() == "GET") {
+      components.add("-X ${options.method}");
+    }
+
+    if (options.headers != null) {
+      options.headers.forEach((k, v) {
+        if (k != "Cookie") {
+          components.add("-H \"$k: $v\"");
+        }
+      });
+    }
+
+    var data = json.encode(options.body);
+    if (data != null && data != "null") {
+      components.add("-d $data");
+    }
+
+    components.add("\"${options.url.toString()}\"");
+
+    return components.join('\\\n\t');
+  } catch (err) {
+    debugPrint(err.toString());
+    return "error export";
   }
-
-  if (options.headers != null) {
-    options.headers.forEach((k, v) {
-      if (k != "Cookie") {
-        components.add("-H \"$k: $v\"");
-      }
-    });
-  }
-
-  var data = json.encode(options.body);
-  if (data != null && data != "null") {
-    components.add("-d $data");
-  }
-
-  components.add("\"${options.url.toString()}\"");
-
-  return components.join('\\\n\t');
 }
